e56d6ddc050bb5d6900dea709c5f556dd8d0dfc2
slobodyanik <slobodyanik.vyacheslav@gmail.com>
2017-07-08 14:35:17 +0300

    completed function 'areIntersected'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-﻿
+﻿let rectangleA = {
+	top: (Integer),
+	left: (Integer),
+	width: (Integer),
+	height: (Integer)
+}
+let rectangleB = {
+	top: (Integer),
+	left: (Integer),
+	width: (Integer),
+	height: (Integer)
+}
+function areIntersected(rectangleA, rectangleB) {
+	if (rectangleA.top + rectangleA.height) <= rectangleB.top ||
+		(rectangleA.left + rectangleA.width) >= rectangleB.left ||
+		rectangleA.top <= (rectangleB.top + rectangleB.height) ||
+		{
+			return true;
+		}
+	else {
+		return false;
+	}
+}
+function filterVisible(rectangleA, [])
